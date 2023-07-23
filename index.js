8839d1b129fdd9263ad689555366275dfdd8821f
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-23 20:02:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data))
+  .catch(error => console.log(error))
 }
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
